2b3fadeb3f8a04622c43f4369389d52ae7774315
Jason <JPriskomatis@gmail.com>
2024-12-15 15:20:41 +0200

    Fix: Player passing through doors colliderss

---
+++ Assets/Scripts/Globals/GlobalConstants.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GlobalSpace
+{
+    public static class GlobalConstants
+    {
+        public static KeyCode OPEN_DOOR_KEY = KeyCode.E;
+    }
+
+}
---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,60 @@
+using EJETAGame;
+using GlobalSpace;
+using System.Collections;
+using UnityEngine;
+
+namespace ObjectSpace
+{
+    public class Door : MonoBehaviour, IInteractable
+    {
+        [Header("Door Settings")]
+        [SerializeField] private float rotationDuration;
+        private bool isOpen;
+        private float targetRotation = 90f;
+        private bool isRotating;
+
+        public void Interact()
+        {
+            if (Input.GetKeyDown(GlobalConstants.OPEN_DOOR_KEY) && !isOpen)
+            {
+                StartCoroutine(OpenDoor());
+            }
+        }
+
+        public void OnInteractEnter()
+        {
+            InteractionText.instance.SetText("Open Door");
+        }
+
+        public void OnInteractExit()
+        {
+            Debug.Log("exit");
+        }
+
+        //Open Door Method;
+        private IEnumerator OpenDoor()
+        {
+            isOpen = true;
+            if (!isRotating)
+            {
+                
+                isRotating = true;
+                float startRotation = transform.rotation.eulerAngles.y;
+                float timeElapsed = 0f;
+
+                while (timeElapsed < rotationDuration)
+                {
+                    float newRotation = Mathf.Lerp(startRotation, startRotation + targetRotation, timeElapsed / rotationDuration);
+                    transform.rotation = Quaternion.Euler(0, newRotation, 0);
+                    timeElapsed += Time.deltaTime;
+                    yield return null;
+                }
+
+                // Ensure the door is exactly at the target rotation at the end
+                transform.rotation = Quaternion.Euler(0, startRotation + targetRotation, 0);
+                isRotating = false;
+            }
+        }
+    }
+
+}

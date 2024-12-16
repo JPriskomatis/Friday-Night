2420d954dff2a93b442928cc814e0988e8193592
Jason <JPriskomatis@gmail.com>
2024-12-16 22:15:36 +0200

    Add: simple change culling musk layer + VHS audio

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+namespace PlayerSpace
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        [Header("Camera Settings")]
+        [SerializeField] private Camera mainCamera;
+        public string layerName = "Monster";
+        int layerIndex;
+
+        private void Start()
+        {
+            //Finds the specific layer we want to use;
+            layerIndex = LayerMask.NameToLayer(layerName);
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                AddMonsterLayer();
+            }
+        }
+        private void AddMonsterLayer()
+        {
+            mainCamera.cullingMask |= 1 << layerIndex;
+
+            StartCoroutine(RemoveMonsterLayer());
+        }
+
+        IEnumerator RemoveMonsterLayer()
+        {
+            yield return new WaitForSeconds(2f);
+            mainCamera.cullingMask &= ~(1 << layerIndex);
+        }
+    }
+
+}

d582fed5839cad19c64f1bc06826135c81335438
Jason <JPriskomatis@gmail.com>
2025-01-05 19:40:30 +0200

    Add: Glitch effect shader

--- Assets/Scripts/Monster/Jumpscare.cs
+++ Assets/Scripts/Monster/Jumpscare.cs
@@ -5,8 +5,27 @@ namespace MonsterSpace
 {
     public class Jumpscare : MonoBehaviour
     {
+        [Header("Material Settings")]
+        [SerializeField] private Material mat;
+        [SerializeField] private float noiseAmount;
+        [SerializeField] private float glitchStrength;
+        [SerializeField] private float scanlinesStrength;
+
+        [Header("Jumpscare Components")]
         [SerializeField] private CanvasGroup blackScreen;
         [SerializeField] private GameObject monster;
+
+        private void Update()
+        {
+            StartGlitchEffect();
+        }
+        public void StartGlitchEffect()
+        {
+            mat.SetFloat("_NoiseAmount", noiseAmount);
+            mat.SetFloat("_GlitchStrength", glitchStrength);
+            mat.SetFloat("_ScanLinesStrength", scanlinesStrength);
+        }
+
         public void StopController()
         {
             PlayerController.Instance.StopMovement();
@@ -18,6 +37,7 @@ namespace MonsterSpace
             blackScreen.DOFade(1, 0.2f);
             monster.SetActive(true);
         }
+
     }
 
 }

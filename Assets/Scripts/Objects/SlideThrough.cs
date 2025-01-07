0e9203e79d91bd8df28f29a767a45d07871e2dd5
Jason <JPriskomatis@gmail.com>
2025-01-07 23:51:19 +0200

    Smooth in into slide animations

--- Assets/Scripts/Objects/SlideThrough.cs
+++ Assets/Scripts/Objects/SlideThrough.cs
@@ -1,3 +1,5 @@
+using Codice.CM.Common;
+using DG.Tweening;
 using ObjectSpace;
 using System.Collections;
 using UnityEngine;
@@ -7,14 +9,35 @@ public class SlideThrough : InteractableItem
     [SerializeField] Animator anim;
     [SerializeField] string animationName;
     [SerializeField] float delayRootMotion;
+    [SerializeField] Transform animPosition;
+
     protected override void BeginInteraction()
     {
+        //Move character to position;
+        MoveAndTriggerWhenComplete(animPosition.position, 4f, animationName);
+
+        
+    }
+
+    public void MoveAndTriggerWhenComplete(Vector3 targetPositionVector, float speed, string animationName)
+    {
+        PlayerController.Instance.MoveToPosition(animPosition, speed);
+        StartCoroutine(WaitForMovementToComplete(targetPositionVector, animationName));
+    }
+
+    private IEnumerator WaitForMovementToComplete(Vector3 animPosition, string animationName)
+    {
+        float threshold = 0.3f;
+        while (Vector3.Distance(PlayerController.Instance.transform.position, animPosition) > threshold)
+        {
+            yield return null;  // Wait until the next frame
+        }
+
+        // Actions after reaching the position
         DisableRootMotion();
         anim.SetTrigger(animationName);
         PlayerController.Instance.GetCameraPos();
     }
-
-    
 
     IEnumerator EnableRootMotion()
     {
@@ -22,6 +45,7 @@ public class SlideThrough : InteractableItem
         anim.applyRootMotion = true;
         canInteractWith = true;
         PlayerController.Instance.ResetCameraPos();
+        PlayerController.Instance.ResetMovement();
     }
 
     private void DisableRootMotion()

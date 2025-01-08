1c934958ab17225ca25aaf7f0da62c76e4736c9d
Jason <JPriskomatis@gmail.com>
2025-01-08 22:59:31 +0200

    Add: camera Menu first with static bg

--- Assets/Scripts/OpeningScene/OpeningMenu.cs
+++ Assets/Scripts/OpeningScene/OpeningMenu.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace OpeningScene
@@ -6,6 +7,7 @@ namespace OpeningScene
     public class OpeningMenu : MonoBehaviour
     {
         [SerializeField] private Animator cameraAnim, sceneCameraAnim;
+        [SerializeField] private GameObject menuObject;
 
         public static event Action OnClick;
         private void OnMouseDown()
@@ -13,9 +15,15 @@ namespace OpeningScene
             Debug.Log($"{gameObject.name} clicked!");
             cameraAnim.SetTrigger("Zoom");
             sceneCameraAnim.SetTrigger("Open");
+            StartCoroutine(ActivateMenu());
             OnClick?.Invoke();
         }
 
+        IEnumerator ActivateMenu()
+        {
+            yield return new WaitForSeconds(2.5f);
+            menuObject.SetActive(true);
+        }
 
     }
 

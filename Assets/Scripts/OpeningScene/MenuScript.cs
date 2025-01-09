35c9ab081e88439820a22c4316646b8237272e6d
Jason <JPriskomatis@gmail.com>
2025-01-09 20:54:16 +0200

    Fix: Seperated scene transition logic

--- Assets/Scripts/OpeningScene/MenuScript.cs
+++ Assets/Scripts/OpeningScene/MenuScript.cs
@@ -8,15 +8,13 @@ public class MenuScript : MonoBehaviour
     [SerializeField] private GameObject startGamePanel;
     private Animator anim;
     private CanvasGroup blackPanel;
-    private bool canTransition;
+
 
     private void Start()
     {
         anim = Camera.main.gameObject.GetComponent<Animator>();
         blackPanel = Camera.main.GetComponentInChildren<CanvasGroup>();
 
-        //Load next scene;
-        StartCoroutine(LoadScene());
     }
     public void StartGamePanel()
     {
@@ -27,35 +25,7 @@ public class MenuScript : MonoBehaviour
     {
         anim.SetTrigger("Enter");
         blackPanel.DOFade(1, 0.5f);
-
-        StartCoroutine(TransitionToScene());
     }
 
-    IEnumerator LoadScene()
-    {
-        yield return null;
-
-        AsyncOperation asyncOpe = SceneManager.LoadSceneAsync(0);
-        asyncOpe.allowSceneActivation = false;
-
-        while (!asyncOpe.isDone)
-        {
-            Debug.Log(asyncOpe.progress);
-            if (asyncOpe.progress >= 0.9f)
-            {
-                canTransition = true;
-                break;
-            }
-            yield return null;
-        }
-    }
-
-    IEnumerator TransitionToScene()
-    {
-        if (canTransition)
-        {
-            yield return new WaitForSeconds(2f);
-            SceneManager.LoadScene(0, LoadSceneMode.Single);
-        }
-    }
+    
 }

140e09574d84821ef2646dae932aa146db9ef0c4
JPriskomatis <JPriskomatis@gmail.com>
2025-02-06 22:21:43 +0200

    Add: Final painting drop after hiting trigger

---
+++ Assets/Scripts/Triggers/Triggers/PaintingsTrigger.cs
@@ -0,0 +1,40 @@
+using ObjectSpace;
+using TriggerSpace;
+using UnityEngine;
+
+namespace TriggerSpace
+{
+    public class PaintingsTrigger : FloorTrigger
+    {
+
+        
+        [SerializeField] Animator anim;
+
+        private void OnEnable()
+        {
+            FinalPainting.OnFlicker += CanInteract;
+        }
+
+        private void OnDisable()
+        {
+            FinalPainting.OnFlicker -= CanInteract;
+        }
+
+        private void Start()
+        {
+            interactable = false;
+            interactAgain = false;
+        }
+
+        private void CanInteract()
+        {
+            interactable = true;
+        }
+        protected override void InitiateAction()
+        {
+            anim.SetTrigger("boom");
+            Debug.Log("Boom");
+        }
+    }
+
+}

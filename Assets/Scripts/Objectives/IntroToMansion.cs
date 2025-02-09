4d50f1d0f957da43f3272642debfa8d870223b8b
JPriskomatis <JPriskomatis@gmail.com>
2025-02-09 17:47:27 +0200

    Intro text sequence

--- Assets/Scripts/Objectives/IntroToMansion.cs
+++ Assets/Scripts/Objectives/IntroToMansion.cs
@@ -24,13 +24,14 @@ public class IntroToMansion : MonoBehaviour
         {
             blackScreen.SetActive(true);
             vhsAudio.SetActive(false);
-            
+
 
             yield return new WaitForSeconds(3f);
+
+
+
             //Entrance to Mansion audio;
-            audioSource.clip = entranceAudio;
-            audioSource.Play();
-            yield return new WaitForSeconds(5f);
+            
 
             PlayerThoughts.Instance.showTextDuration = 5f;
             for (int i = 0; i < introText.Length; i++)
@@ -38,29 +39,36 @@ public class IntroToMansion : MonoBehaviour
                 PlayerThoughts.Instance.SetText(introText[i]);
                 yield return new WaitForSeconds(PlayerThoughts.Instance.showTextDuration);
 
-                if (i == 0)
-                {
-                    //Turning the camera on audio;
-                    audioSource.clip = cameraAudio;
-                    audioSource.Play();
-
-                    yield return new WaitForSeconds(0.5f);
-
-                    //Our camera is on so we enable the ambient audio;
-                    vhsAudio.SetActive(true);
-                    blackScreen.GetComponent<CanvasGroup>().alpha = 0;
-
-                    
-                }
             }
+
+            EntranceDoorAudio();
+            yield return new WaitForSeconds(5f);
 
             //Once the monologue ends, we showcase the objective to the player;
             ObjectiveManager.Instance.ShowObjective(objectiveText);
-        } else
+
+
+            //Turning the camera on audio;
+            audioSource.clip = cameraAudio;
+            audioSource.Play();
+
+            yield return new WaitForSeconds(0.5f);
+
+            //Our camera is on so we enable the ambient audio;
+            vhsAudio.SetActive(true);
+            blackScreen.GetComponent<CanvasGroup>().alpha = 0;
+        }
+        else
         {
             blackScreen.GetComponent<CanvasGroup>().alpha = 0;
         }
         
         
+    }
+
+    private void EntranceDoorAudio()
+    {
+        audioSource.clip = entranceAudio;
+        audioSource.Play();
     }
 }

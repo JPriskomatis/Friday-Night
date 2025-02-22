1fd1a10ba4afc08bdbc6edb0a8f13024919c3962
JPriskomatis <JPriskomatis@gmail.com>
2025-02-22 22:17:00 +0200

    Add: TV Room sequence to library move

--- Assets/Scripts/Objects/TVScreenVOICE.cs
+++ Assets/Scripts/Objects/TVScreenVOICE.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections;
+using AudioSpace;
 using PlayerSpace;
 using UISpace;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Video;
 
 namespace VoiceSpace
@@ -9,6 +13,13 @@ namespace VoiceSpace
     {
         [SerializeField] private VideoPlayer videoPlayer;
         [SerializeField] private VideoClip videoClip;
+        [SerializeField] private UnityEvent OnAnimationBegin;
+        
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip clip;
+
+        public static event Action OnOpenBookcase;
+
         public override void AddDictionaryFunctions()
         {
             voiceActions.Add(speechWords[0], WhatDoYouWant);
@@ -17,6 +28,24 @@ namespace VoiceSpace
         {
             Debug.Log("I tell you what I want");
             videoPlayer.clip = videoClip;
+
+            OnAnimationBegin?.Invoke();
+            this.GetComponent<SphereCollider>().enabled = false;
+
+            audioSource.Play();
+            Audio.Instance.PlayAudioFadeIn(clip);
+
+            PlayerController.Instance.ResetMovement();
+            HintMessage.Instance.RemoveMessage();
+
+            StartCoroutine(DelayForBookscase());
+        }
+
+        IEnumerator DelayForBookscase()
+        {
+            yield return new WaitForSeconds(2.5f);
+            OnOpenBookcase?.Invoke();
+            this.enabled = false;
         }
 
     }

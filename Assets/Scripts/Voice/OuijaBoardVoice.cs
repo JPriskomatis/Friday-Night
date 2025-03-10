db134456422a1b08752c9d8c103766a9e1d88064
JPriskomatis <JPriskomatis@gmail.com>
2025-03-10 23:12:42 +0200

    Fix: Changed from keyword recognition to dictationRecognizer

--- Assets/Scripts/Voice/OuijaBoardVoice.cs
+++ Assets/Scripts/Voice/OuijaBoardVoice.cs
@@ -103,7 +103,10 @@ namespace VoiceSpace
             //Where are you
             voiceActions.Add(speechWords[10], WhereAreYou);
             voiceActions.Add(speechWords[11], WhereAreYou);
-            Debug.Log("Added: " + speechWords[10]);
+            voiceActions.Add(speechWords[12], WhereAreYou);
+            voiceActions.Add(speechWords[13], WhereAreYou);
+            voiceActions.Add(speechWords[14], WhereAreYou);
+            voiceActions.Add(speechWords[15], WhereAreYou);
         }
 
         private void WhoAreYou()
